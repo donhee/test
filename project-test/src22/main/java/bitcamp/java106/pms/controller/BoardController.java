20947f3b7b47ee0cb4388041d4aadacc5bddc8df
donhee <231313do@gmail.com>
2018-10-31 15:03:35 +0900

    src22 - Dao에 대해 geralization을 적용하라!

---
+++ project-test/src22/main/java/bitcamp/java106/pms/controller/BoardController.java
@@ -0,0 +1,125 @@
+package bitcamp.java106.pms.controller;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+import bitcamp.java106.pms.dao.BoardDao;
+import bitcamp.java106.pms.domain.Board;
+import bitcamp.java106.pms.util.Console;
+
+public class BoardController implements Controller {
+    Scanner keyScan;
+    BoardDao boardDao;
+    
+    public BoardController(Scanner scanner, BoardDao boardDao) {
+        this.keyScan = scanner;
+        this.boardDao = boardDao;
+    }
+    
+    public void service(String menu, String option) {
+        if (menu.equals("board/add")) {
+            this.onBoardAdd();
+        } else if (menu.equals("board/list")) {
+            this.onBoardList();
+        } else if (menu.equals("board/view")) {
+            this.onBoardView(option);
+        } else if (menu.equals("board/update")) {
+            this.onBoardUpdate(option);
+        } else if (menu.equals("board/delete")) {
+            this.onBoardDelete(option);
+        } else {
+            System.out.println("명령어가 올바르지 않습니다.");
+        }
+    }
+    
+    void onBoardAdd() {
+        System.out.println("[게시물 입력]");
+        
+        Board board = new Board();
+
+        System.out.print("제목? ");
+        board.setTitle(keyScan.nextLine());
+        
+        System.out.print("내용? ");
+        board.setContent(keyScan.nextLine());
+        
+        System.out.print("등록일? ");
+        board.setCreateDate(Date.valueOf(keyScan.nextLine()));
+        
+        boardDao.insert(board);
+    }
+    
+    void onBoardList() {
+        System.out.println("[게시물 목록]");
+        Board[] boards = boardDao.list();
+        for (Board board : boards) {
+            System.out.printf("%d, %s, %s\n",
+                    board.getNo(), board.getTitle(), board.getCreateDate());
+        }
+    }
+    
+    void onBoardView(String option) {
+        System.out.println("[게시물 조회]");
+        
+        if (option == null) {
+            System.out.println("번호를 입력하시기 바랍니다.");
+            return;
+        }
+        
+        Board board = boardDao.get(Integer.parseInt(option));
+        
+        if (board == null) {
+            System.out.println("해당 게시물 번호가 없습니다.");
+        } else {
+            System.out.printf("제목: %s\n", board.getTitle());
+            System.out.printf("내용: %s\n", board.getContent());
+            System.out.printf("시작일: %s\n", board.getCreateDate());
+        }
+    }
+    
+    void onBoardUpdate(String option) {
+        System.out.println("[게시물 변경]");
+        if (option == null) {
+            System.out.println("번호를 입력하시기 바랍니다.");
+            return;
+        }
+        
+        Board board = boardDao.get(Integer.parseInt(option));
+        if (board == null) {
+            System.out.println("해당 게시물 번호가 없습니다.");
+        } else {
+            Board updateBoard = new Board();
+            System.out.printf("제목(%s)? ", board.getTitle());
+            updateBoard.setTitle(keyScan.nextLine());
+            System.out.printf("내용(%s)? ", board.getContent());
+            updateBoard.setContent(keyScan.nextLine());
+            System.out.printf("시작일(%s)? ", board.getCreateDate());
+            updateBoard.setCreateDate(Date.valueOf(keyScan.nextLine()));
+
+            updateBoard.setNo(board.getNo());
+            boardDao.update(updateBoard);
+            System.out.println("변경하였습니다.");
+        }
+    }
+
+    void onBoardDelete(String option) {
+        System.out.println("[게시물 삭제]");
+        if (option == null) {
+            System.out.println("번호를 입력하시기 바랍니다.");
+            return;
+        }
+        
+        int i = Integer.parseInt(option);
+        Board board = boardDao.get(i);
+        
+        if (board == null) {
+            System.out.println("해당 게시물이 없습니다.");
+        } else {
+            if (Console.confirm("정말 삭제하시겠습니까?")) {
+                boardDao.delete(i);
+                System.out.println("삭제하였습니다.");
+            }
+        }
+    }
+    
+}
